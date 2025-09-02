d96b12cf14b2a991797217e475a57e51bb9f4acf
alicegmn <alice.eriksson.561@gmail.com>
2025-09-02 16:28:27 +0200

    Initiated frontend PWA project with Vite + React + TypeScript + Tailwind + shadcn/ui

---
+++ pwa/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-center justify-center text-3xl font-bold">
+        Welcome to the top logistics services provided by the Chas Advance team!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
