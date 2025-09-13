de8d002c9f33e4c1fdd617292b4513e6e2c15af3
alicegmn <alice.eriksson.561@gmail.com>
2025-09-13 20:32:13 +0200

    feat(auth): implement authentication (with mock login) change in common/authApi later

---
+++ common/src/index.ts
@@ -0,0 +1,18 @@
+import Button from "./components/Button";
+
+// types
+export * from "./types/auth";
+
+// lib
+export * from "./lib/http";
+export * from "./lib/authApi";
+export * from "./lib/storage";
+
+// auth
+export { AuthProvider, useAuth } from "./hooks/auth/AuthProvider";
+export { default as ProtectedRoute } from "./hooks/auth/ProtectedRoute";
+
+// components
+export * from "./components/form/Input";
+export * from "./components/form/Input";
+export * from "./components/Card";
