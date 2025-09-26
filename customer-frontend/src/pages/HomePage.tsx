d5269026e4fbc6e007d21a7e5ad91f74c3bb15dc
alicegmn <alice.eriksson.561@gmail.com>
2025-09-26 11:32:36 +0200

    feat(routing): implement routing and layout for customer

--- customer-frontend/src/pages/HomePage.tsx
+++ customer-frontend/src/pages/HomePage.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@frontend/common";
+import { Scan } from "lucide-react";
+
 export default function HomePage() {
-  return (
-    <div className="text-center">
-      <h1>This is the homepage</h1>
-    </div>
-  );
+	return (
+		<div className="text-center">
+			<h1>Hi, {name}</h1>
+			<Button onClick={() => alert("Button clicked!")}>My parcels</Button>
+			<Button onClick={() => alert("Button clicked!")}></Button>
+		</div>
+	);
 }
