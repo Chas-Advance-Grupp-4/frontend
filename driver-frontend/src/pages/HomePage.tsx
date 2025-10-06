520a49ae17d76f557b0a79d5a639132fd76eddee
Rhibro <rhibron@gmail.com>
2025-10-06 14:14:39 +0200

    added nav to driver home page

--- driver-frontend/src/pages/HomePage.tsx
+++ driver-frontend/src/pages/HomePage.tsx
@@ -1,7 +1,13 @@
+import BottomNav from "@/components/layout/BottomNav";
+import OptionsNav from "@/components/layout/OptionsNav";
+
 export default function HomePage() {
   return (
     <div className="text-center">
       <h1>This is the homepage</h1>
+      <OptionsNav />
+      
+      <BottomNav />
     </div>
   );
 }
