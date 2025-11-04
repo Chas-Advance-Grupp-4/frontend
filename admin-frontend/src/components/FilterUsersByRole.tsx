99051e8a9e1db51f9c8e00f2c33aef0c1716d284
neriiman <neriman@live.se>
2025-11-04 10:35:06 +0100

    feat: refactor FilterUsersByRole to use Select and Option components; add role alert to ToastNotification

--- admin-frontend/src/components/FilterUsersByRole.tsx
+++ admin-frontend/src/components/FilterUsersByRole.tsx
@@ -1,3 +1,5 @@
+import Option from "../../../common/src/components/form/Option";
+import Select from "../../../common/src/components/form/Select";
 import { Role } from "common/src/types/auth";
 import React from "react";
 interface Props {
@@ -8,15 +10,19 @@ interface Props {
 const FilterUsersByRole = ({ value, onChange }: Props) => {
   return (
     <>
-      <select className="border rounded p-2 " value={value} onChange={onChange}>
-        <option value="no filter">
+      <Select
+        value={value}
+        onChange={onChange}
+        // className="w"
+      >
+        <Option value="no filter">
           Filter users by role
           {/* <AdjustmentsVerticalIcon className="h-6 w-6" /> */}
-        </option>
-        <option value="customer">Customer</option>
-        <option value="admin">Admin</option>
-        <option value="driver">Driver</option>
-      </select>
+        </Option>
+        <Option value="customer">Customer</Option>
+        <Option value="admin">Admin</Option>
+        <Option value="driver">Driver</Option>
+      </Select>
     </>
   );
 };
